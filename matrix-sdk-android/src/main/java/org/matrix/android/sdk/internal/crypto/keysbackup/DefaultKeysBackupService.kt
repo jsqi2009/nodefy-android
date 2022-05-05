4298da3ba75aab41fbaa166ce23a5d34c4bd3d42
Benoit Marty <benoit@matrix.org>
2022-05-05 19:00:03 +0200

    Method `onSecretKeyGossip` doe snot have to be in the public interface

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/crypto/keysbackup/DefaultKeysBackupService.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/crypto/keysbackup/DefaultKeysBackupService.kt
@@ -580,7 +580,7 @@ internal class DefaultKeysBackupService @Inject constructor(
         }
     }
 
-    override fun onSecretKeyGossip(secret: String) {
+    fun onSecretKeyGossip(secret: String) {
         Timber.i("## CrossSigning - onSecretKeyGossip")
 
         cryptoCoroutineScope.launch(coroutineDispatchers.io) {
